cb3f7e353b911814af02ef97b402afc1b59004a7
barron9 <barron9@yandex.com>
2018-12-15 18:31:14 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+const baslangicdeger ={
+count:0
 
-ReactDOM.render(<App />, document.getElementById('root'));
+}
+const reducer= (state = baslangicdeger,action) =>{
+switch(action.type){
+    case 'INC':
+    return {
+        count:state.count+1
+    }
+    default:
+    return state
+}
+}
+const store= createStore(reducer)
+console.log(store.getState())
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//setInterval(()=>{
+//store.dispatch({type:'INC'})
+//console.log(store.getState())
+
+//},10)
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
